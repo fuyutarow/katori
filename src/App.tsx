e7acd76c66055780d49eb5448a3325cd8814f36a
fuyutarow <fuyutarow@gmail.com>
2020-06-13 20:12:33 +0900

    yarn lint and add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const Counter = () => {
+import {
+  Container,
+  Button,
+} from '@material-ui/core';
 
+const Counter = () => {
   const audioContext = new AudioContext();
   const carrier = audioContext.createOscillator();
   carrier.type = 'sine';
@@ -23,50 +27,69 @@ const Counter = () => {
   carrierGain.connect(audioContext.destination);
 
   carrier.start(0);
-    carrier.disconnect()
+  carrier.disconnect();
   modulator.start(0);
 
   const onAudio = () => {
-    carrier.connect(audioContext.destination)
-  }
+    carrier.connect(audioContext.destination);
+  };
 
   const offAudio = () => {
-    carrier.disconnect()
-  }
+    carrier.disconnect();
+  };
 
   const setHigh = () => {
     carrier.frequency.value = 15000;
-  }
+  };
 
   const setLow = () => {
     carrier.frequency.value = 220;
-  }
+  };
 
   return (
     <div>
-      <div>
-
-        <button onClick={onAudio} >on</button>
-        <button onClick={offAudio}>off</button>
-      </div>
-      <div>
-        <button onClick={setHigh} >high</button>
-        <button onClick={setLow} >low</button>
-
-      </div>
+      <Button
+        variant="outlined"
+        disableElevation
+        onClick={() => {
+          onAudio();
+          setLow();
+        }}
+      >
+          蚊誘引
+      </Button>
+      <Button
+        variant="outlined"
+        disableElevation
+        onClick={() => {
+          onAudio();
+          setHigh();
+        }}
+      >
+          蚊忌避
+      </Button>
+      <Button
+        variant="outlined"
+        disableElevation
+        onClick={() => {
+          offAudio();
+        }}
+      >
+          音停止
+      </Button>
     </div>
   );
-}
-
+};
 
 const App = () => {
 
-
   return (
     <div className="App">
+      {/* <Container component="main" maxWidth="xs"> */}
+      <img src="/icons/512x512.png" className="App-logo" alt="logo" />
       <Counter />
     </div>
   );
-}
+};
 
 export default App;
